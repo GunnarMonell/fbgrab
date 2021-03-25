4966dd25b88a40967d9c15c2fcf79c1a94bf13dd
Gunnar Monell <gunnar@vireone.com>
2021-03-25 10:16:57 +0100

    Fbgrab 1.5, support for ignoring alpha channel.

--- fbgrab.c
+++ fbgrab.c
@@ -26,7 +26,7 @@
 #include <zlib.h>
 #include <linux/fb.h> /* to handle framebuffer ioctls */
 
-#define	VERSION	"1.4"
+#define	VERSION	"1.5"
 #define	DEFAULT_FB	"/dev/fb0"
 #define MAX_LEN 512
 #define UNDEFINED -1
